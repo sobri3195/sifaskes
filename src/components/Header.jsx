a7b41a404e9e3ef22ddc9a053efb448c71822abf
Muhammad Sobri Maulana <sobri3195@gmail.com>
2026-04-24 17:51:14 +0700

    Add branding assets and improve responsive map UX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import logo from '../assets/sifaskes-logo.svg';
+
 function Header() {
   return (
     <header className="header">
-      <div>
-        <h1>SiFaskes</h1>
-        <p>Sistem Informasi Fasilitas Kesehatan</p>
+      <div className="header-brand">
+        <img src={logo} alt="Logo SiFaskes" className="header-logo" />
+        <div>
+          <h1>SiFaskes</h1>
+          <p>Sistem Informasi Fasilitas Kesehatan</p>
+        </div>
       </div>
       <span className="tagline">Peta Profil RSAU & FKTP Jajaran TNI AU</span>
     </header>
